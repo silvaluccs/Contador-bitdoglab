da3189cf6930fdb3aec9d61e0c8e7653c1406d99
Lucas Silva <kucasss19@gmail.com>
2025-01-29 10:39:41 -0300

    implementação função inicializar botoes a e b

--- main.c
+++ main.c
@@ -4,8 +4,11 @@
 #include "ws2812.pio.h"
 
 const uint pino_led_vermelho = 13;
+const uint pino_botao_a = 5;
+const uint pino_botao_b = 6;
 
 void setup_led_vermelho();
+void setup_botoes();
 
 int main()
 {
@@ -26,7 +29,22 @@ int main()
 void setup_led_vermelho() {
 
     gpio_init(pino_led_vermelho); // inicializando o pino
-    gpio_set_dir(pino_led_vermelho, GPIO.OUT); // definindo como saida
+    gpio_set_dir(pino_led_vermelho, GPIO_OUT); // definindo como saida
     gpio_put(pino_led_vermelho, false); // deixando desligado inicialmente
+
+}
+
+/*
+* Função para inicializar os botões a e b
+*/
+void setup_botoes() {
+
+    gpio_init(pino_botao_a); // inicializando o pino
+    gpio_set_dir(pino_botao_a, GPIO_IN); // defininfo como entrada
+    gpio_pull_up(pino_botao_a); // ativando resistores internos
+
+    gpio_init(pino_botao_b); // inicializando o pino
+    gpio_set_dir(pino_botao_b, GPIO_IN); // definindo como entrada
+    gpio_pull_up(pino_botao_b); // ativando resistores internos
 
 }
