a610da82140e403ec6553707da853359ec8b8038
Lucas Silva <kucasss19@gmail.com>
2025-01-29 10:34:33 -0300

    implementação função inicializar led vermelho

--- main.c
+++ main.c
@@ -1,40 +1,32 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
+#include "ws2812.pio.h"
 
-#include "blink.pio.h"
+const uint pino_led_vermelho = 13;
 
-void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
-    blink_program_init(pio, sm, offset, pin);
-    pio_sm_set_enabled(pio, sm, true);
-
-    printf("Blinking pin %d at %d Hz\n", pin, freq);
-
-    // PIO counter program takes 3 more cycles in total than we pass as
-    // input (wait for n + 1; mov; jmp)
-    pio->txf[sm] = (125000000 / (2 * freq)) - 3;
-}
-
-
+void setup_led_vermelho();
 
 int main()
 {
     stdio_init_all();
 
-    // PIO Blinking example
-    PIO pio = pio0;
-    uint offset = pio_add_program(pio, &blink_program);
-    printf("Loaded program at %d\n", offset);
-    
-    #ifdef PICO_DEFAULT_LED_PIN
-    blink_pin_forever(pio, 0, offset, PICO_DEFAULT_LED_PIN, 3);
-    #else
-    blink_pin_forever(pio, 0, offset, 6, 3);
-    #endif
-    // For more pio examples see https://github.com/raspberrypi/pico-examples/tree/master/pio
+    // inicializando o led vermelho
+    setup_led_vermelho();
 
     while (true) {
         printf("Hello, world!\n");
         sleep_ms(1000);
     }
+}
+
+/*
+* Função para inicializar o led vermelho
+*/
+void setup_led_vermelho() {
+
+    gpio_init(pino_led_vermelho); // inicializando o pino
+    gpio_set_dir(pino_led_vermelho, GPIO.OUT); // definindo como saida
+    gpio_put(pino_led_vermelho, false); // deixando desligado inicialmente
+
 }
