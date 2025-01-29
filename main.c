b85482c92a56f192f29342a950f32d715c393e34
Lucas Silva <kucasss19@gmail.com>
2025-01-29 10:58:44 -0300

    implementação matriz de leds

--- main.c
+++ main.c
@@ -3,25 +3,37 @@
 #include "hardware/pio.h"
 #include "ws2812.pio.h"
 
+
 const uint pino_led_vermelho = 13;
 const uint pino_botao_a = 5;
 const uint pino_botao_b = 6;
 
+const uint numero_pixels = 25;
+const uint pino_matriz_leds = 7;
+const bool IS_RGBW = false;
+
+
 void setup_led_vermelho();
 void setup_botoes();
+void setup_matriz_leds();
+
+static inline void colocar_pixel(uint32_t pixel_rgb);
+static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b);
+void desenhar_numero_na_matriz_de_leds(uint8_t r, uint8_t g, uint8_t b, uint numero, bool *frame_numero_atual);
 
 int main()
 {
     stdio_init_all();
 
-    // inicializando o led vermelho
-    setup_led_vermelho();
+    setup_led_vermelho(); // inicializando o led vermelho
+    setup_botoes(); // inicializando os botoes a e b
 
     while (true) {
         printf("Hello, world!\n");
         sleep_ms(1000);
     }
 }
+
 
 /*
 * Função para inicializar o led vermelho
@@ -33,6 +45,7 @@ void setup_led_vermelho() {
     gpio_put(pino_led_vermelho, false); // deixando desligado inicialmente
 
 }
+
 
 /*
 * Função para inicializar os botões a e b
@@ -47,4 +60,56 @@ void setup_botoes() {
     gpio_set_dir(pino_botao_b, GPIO_IN); // definindo como entrada
     gpio_pull_up(pino_botao_b); // ativando resistores internos
 
+}
+
+/*
+* Função para inicializar a matriz de leds da placa
+*/
+void setup_matriz_leds() {
+
+    PIO pio = pio0;
+    int sm = 0;
+    uint offset = pio_add_program(pio, &ws2812_program);
+
+    ws2812_program_init(pio, sm, offset, pino_matriz_leds, 800000, IS_RGBW);
+}
+
+
+/*
+* Função para enviar o pixel para a matriz de leds
+*/
+static inline void colocar_pixel(uint32_t pixel_rgb) {
+    pio_sm_put_blocking(pio0, 0, pixel_rgb << 8u);
+}
+
+
+/*
+* Função para converter os valores rgb
+*/
+static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b)
+{
+    return ((uint32_t)(r) << 8) | ((uint32_t)(g) << 16) | (uint32_t)(b);
+}
+
+
+/*
+* Função para desenhar um número especifico na matriz de leds
+*/
+void desenhar_numero_na_matriz_de_leds(uint8_t r, uint8_t g, uint8_t b, uint numero, bool *frame_numero_atual)
+{
+    // Define a cor com base nos parâmetros fornecidos
+    uint32_t cor = urgb_u32(r, g, b);
+
+    // Define todos os LEDs com a cor especificada
+    for (int i = 0; i < numero_pixels; i++)
+    {
+        if (frame_numero_atual[i])
+        {
+            colocar_pixel(cor); // Liga o LED com um no buffer
+        }
+        else
+        {
+            colocar_pixel(0);  // Desliga os LEDs com zero no buffer
+        }
+    }
 }
